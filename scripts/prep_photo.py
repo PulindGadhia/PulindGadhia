cdb0722200a8a0bd1369891edea37558620389b4
Pulind Gadhia <gadhiapulind@gmail.com>
2026-07-19 18:29:51 +0530

    Create modular ASCII rendering architecture

---
+++ scripts/prep_photo.py
@@ -0,0 +1,7 @@
+from ascii.prep import preprocess_image
+
+if __name__ == "__main__":
+    preprocess_image(
+        input_path="assets/profile.jpg",
+        output_path="assets/profile-prepped.png"
+    )
